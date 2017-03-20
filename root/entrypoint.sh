9ed759a3a1da438d8356b06d4ae9ec00d07f362d
Michael Mano <michael.mano26@gmail.com>
2017-03-21 08:47:05 +1000

    updated init script

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -2,4 +2,7 @@
 /etc/init.d/php7.0-fpm restart
 /etc/init.d/nginx restart
 /etc/init.d/mysql restart
+if [ ! -f "/var/www/index.php" ]; then
+  curl -O https://wordpress.org/latest.zip && unzip ./latest.zip && mv ./wordpress/* ./ && rm -rf ./latest.zip ./wordpress
+fi
 while true; do sleep 1000; done
